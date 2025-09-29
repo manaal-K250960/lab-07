d53ef27758cc43a947a1ea3abe1efeace4c20d0b
Manaal Mehtab <K250960@nu.edu.pk>
2025-09-29 09:53:32 +0500

    Update task1.c

--- task1.c
+++ task1.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 int main(){
-	int a;
-	printf("input a number: ");
+	int a,reverse=0,r;
+	printf("enter a 3 digit number: ");
 	scanf("%d",&a);
-	for(int i=a;i>=1;i--){
-	printf("%d\n",i);
+	while(a!=0){
+		r=a%10;
+		reverse=reverse*10+r;
+		a=a/10;
 	}
+	printf("%d",reverse);
 	return 0;
 }
